c4805bd373046aff13deb775b929cbb02aedd20e
kaushik <kaushikkumar004@gmail.com>
2020-06-01 20:03:16 +0530

    removed joi validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User =require('../models/User');
-const {regesterValidation, loginValidation} =require('../validation')
+//const {regesterValidation, loginValidation} =require('../validation')
 const bcrypt =require('bcryptjs');
 const jwt=require('jsonwebtoken');
 
@@ -8,8 +8,8 @@ const jwt=require('jsonwebtoken');
 router.post('/register',async (req,res)=>{
 
     //validation of the data
-    const {error,validation} = regesterValidation(req.body);
-    if(error) return res.status(401).send("validation error");
+   // const {error,validation} = regesterValidation(req.body);
+    //if(error) return res.status(401).send("validation error");
 
     // user alredy exist
     const emailExist =await User.findOne({email:req.body.email});
@@ -43,8 +43,8 @@ router.post('/register',async (req,res)=>{
 router.post('/login',async (req,res)=>{
     
     // login validation
-    const {error,validation} = loginValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+  //  const {error,validation} = loginValidation(req.body);
+  //  if(error) return res.status(400).send(error.details[0].message);
 
     //checking if the email exist
     const user =await User.findOne({email: req.body.email});
--- backend/validation.js
+++ backend/validation.js
@@ -1,27 +1,27 @@
-const joi =require('@hapi/joi');
+// const joi =require('@hapi/joi');
 
-// REGISTER VALIDATION WITH JOI
-const regesterValidation = (data)=>{
-const schema = {
-    name : joi.string().required(),
-    email: joi.string().min(6).required().email(),
-    password: joi.string().min(6).required(),
-    city: joi.string().required(),
-    state: joi.string().required(),
-    phone: joi.string().required(),
-    hostelname:joi.string().required()
-};
-return joi.validate(data,schema);
-}
+// // REGISTER VALIDATION WITH JOI
+// const regesterValidation = (data)=>{
+// const schema = {
+//     name : joi.string().required(),
+//     email: joi.string().min(6).required().email(),
+//     password: joi.string().min(6).required(),
+//     city: joi.string().required(),
+//     state: joi.string().required(),
+//     phone: joi.string().required(),
+//     hostelname:joi.string().required()
+// };
+// return schema.validate(data);
+// }
 
-//LOGIN VALIDATION
-const loginValidation = (data)=>{
-    const schema = {
-        email: joi.string().min(6).required().email(),
-        password: joi.string().min(6).required()
-    };
-    return joi.validate(data,schema);
-    }
+// //LOGIN VALIDATION
+// const loginValidation = (data)=>{
+//     const schema = {
+//         email: joi.string().min(6).required().email(),
+//         password: joi.string().min(6).required()
+//     };
+//     return joi.validate(data,schema);
+//     }
 
-module.exports.regesterValidation= regesterValidation;
-module.exports.loginValidation= loginValidation;
+// module.exports.regesterValidation= regesterValidation;
+// module.exports.loginValidation= loginValidation;
